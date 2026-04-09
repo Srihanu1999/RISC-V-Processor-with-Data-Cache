39d5b08d327b6eab9ba8533c2c364ccd05349045
Srihanu <dharanip699@gmail.com>
2026-04-10 02:04:31 +0530

    verilog files

---
+++ RISC - V/Instruction_Memory.v
@@ -0,0 +1,22 @@
+module Instruction_Memory (
+input   wire [31:0]  A,
+output  reg  [31:0]  RD
+);
+   parameter depth = (2**32) -1;
+
+
+
+reg  [31:0] MEM [0:10000];
+
+
+initial
+begin
+  $readmemh("riscvtest.txt",MEM);
+end
+
+always @(*)
+begin
+  RD = MEM [A>>2] ;
+end
+
+endmodule
